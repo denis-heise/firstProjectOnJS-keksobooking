e5573a73ecb3d6f3209306bd92165f7f81783069
Денис <denis.belokopytov.ua@gmail.ua>
2021-06-02 18:33:18 +0300

    Подключил файл main.js в index.html. Добавил две функции в файл main.js, ошибки исправил

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 function randomInteger(min, max) {
   if (min >= 0 && max > 0 && min < max) {
-   let rand = min - 0.5 + Math.random() * (max - min + 1);
-   return Math.round(rand);
-   if (min = max) {
-     let rand = min;
-     return Math.round(rand);
-   }
- }
- return('Задан неверный диапазон! Укажите другие числа.')
- }
- // числа к примеру
- randomInteger(0, 55);
+    if (min === max) {
+      return Math.round(min);
+    }
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
+  return('Задан неверный диапазон! Укажите другие числа.');
+}
+// числа к примеру
+randomInteger(0, 55);
 
 //Источник: https://learn.javascript.ru/task/random-int-min-max
 
@@ -19,15 +18,14 @@ function randomInteger(min, max) {
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 function getRandomArbitrary(min, max) {
   if (min >= 0 && max > 0 && min < max) {
-  return Math.random() * (max - min) + min;
-  if (min = max) {
-    let rand = min;
-    return Math.round(rand);
+    if (min === max) {
+      return Math.round(min);
+    }
+    return Math.random() * (max - min) + min;
   }
-  }
-  return('Задан неверный диапазон! Укажите другие числа.')
+  return('Задан неверный диапазон! Укажите другие числа.');
 }
- // числа к примеру
+// числа к примеру
 getRandomArbitrary(12, 13);
 
- // Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
